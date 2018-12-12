76941707e2e47ca06148e40f2da616832e910948
Mousa Hakim <shadabadel16@gmail.com>
2018-12-12 18:04:24 +0430

    Add csrf token to fetch post call

--- src/Stations.js
+++ src/Stations.js
@@ -3,6 +3,24 @@ import { Dialog, DialogActionsBar } from '@progress/kendo-react-dialogs';
 import { Input } from '@progress/kendo-react-inputs';
 import Modal from './Modal';
 
+// function to get csrfcookie
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+
 class StationForm extends Component {
   constructor(props){
     super(props);
@@ -167,6 +185,7 @@ class Stations extends Component {
         body:JSON.stringify(station),
         headers: {
             "Content-Type": "application/json; charset=utf-8",
+            "X-CSRFToken": getCookie("csrftoken")
         },
       }
     )
